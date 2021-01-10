3c6dfbdfe1189bcb9681752c3b72a89ad5508362
iaia <iaia72160@gmail.com>
2021-01-10 18:23:00 +0900

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+
+
+group = "bot.iaiabot"
+version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+
+rootProject.name = "quiz"
+
+
+include(":mainModule")
+include(":database")
+include(":common")
+include(":util")
+include(":usecase")
+include(":repository")
+include(":entity")
+include(":graphql")
+include(":testmodule")
+
